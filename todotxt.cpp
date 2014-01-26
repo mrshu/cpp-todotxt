e345e96c41eeb04b7c998d0ff1ea33a5ace92025
mr.Shu <mr@shu.io>
2014-01-26 23:32:04 +0100

    better priority handling

--- todotxt.cpp
+++ todotxt.cpp
@@ -8,7 +8,7 @@ TodotxtTask todotxt_parse_task(const char* todo, int id) {
     task.raw_todo = strdup(todo);
 
     if (strlen(todo) > 2) {
-        if (isupper(todo[1])) {
+        if (isupper(todo[1]) && (todo[0] == '(' && todo[2] == ')')) {
             task.priority = todo[1];
         }
     }
