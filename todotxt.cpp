f4890f05eceaadff3756c9054264101b7b049365
mr.Shu <mr@shu.io>
2014-01-29 23:49:29 +0100

    started marking finished tasks

--- todotxt.cpp
+++ todotxt.cpp
@@ -4,15 +4,24 @@ TodotxtTask todotxt_parse_task(const char* todo, int id) {
     TodotxtTask task;
 
     task.id = id;
-    task.finished = false;
     task.raw_todo = strdup(todo);
 
     int clean_todo_start = 0;
+
+    if (strlen(todo) > 3) {
+        if (todo[0] == 'x' && todo[1] == ' ' && todo[2] == ' ') {
+            task.finished = true;
+            clean_todo_start += 3;
+        } else {
+            task.finished = false;
+        }
+    }
+
     if (strlen(todo) > 4) {
         if (isupper(todo[1]) &&
             (todo[0] == '(' && todo[2] == ')' && todo[3] == ' ')) {
             task.priority = todo[1];
-            clean_todo_start = 4;
+            clean_todo_start += 4;
         } else {
             task.priority = '^';
         }
